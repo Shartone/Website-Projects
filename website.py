6d1323968a8678d9143436f89b358d861a5bf2ed
sovamorco <sovamorco@sovamor.co>
2020-10-17 00:50:58 +0300

    Основная структура

---
+++ website.py
@@ -0,0 +1,14 @@
+from credentials import flask_app_secret
+from flask import Flask, render_template
+
+app = Flask(__name__, static_url_path='')
+
+app.secret_key = flask_app_secret
+
+
+@app.route('/')
+def mainpage():
+    return render_template('mainpage.html')
+
+
+app.run(host='0.0.0.0', port=5000)
