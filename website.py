e0e8607b65bd92b4715c0053d3a2584a01ec25b4
sovamorco <sovamorco@sovamor.co>
2020-10-17 04:33:20 +0300

    финал очка

--- website.py
+++ website.py
@@ -1,3 +1,4 @@
+from cheroot.wsgi import Server as WSGIServer
 from credentials import flask_app_secret
 from flask import Flask, render_template
 
@@ -16,4 +17,15 @@ def prices():
     return render_template('mainpage-2.html')
 
 
-app.run(host='0.0.0.0', port=5000, debug=True)
+@app.route('/order')
+def order():
+    return render_template('mainpage-3.html')
+
+
+server = WSGIServer(bind_addr=('0.0.0.0', 8021), wsgi_app=app, numthreads=1)
+try:
+    server.start()
+except KeyboardInterrupt:
+    server.stop()
+except Exception as e:
+    print(e)
